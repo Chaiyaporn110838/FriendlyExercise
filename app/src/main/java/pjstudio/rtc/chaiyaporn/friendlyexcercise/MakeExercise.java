395ec1f54c17f43a861b95a0fb6f0acc6339cd4f
pop <chaip2538@gmail.com>
2016-01-15 14:22:28 +0700

    Create Long Service finish

---
+++ app/src/main/java/pjstudio/rtc/chaiyaporn/friendlyexcercise/MakeExercise.java
@@ -0,0 +1,13 @@
+package pjstudio.rtc.chaiyaporn.friendlyexcercise;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MakeExercise extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_make_exercise);
+    }
+}
